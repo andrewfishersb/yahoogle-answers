a58ddc873dde5645353f8c93b115ea2cd9b11b4d
Andrew Fisher <AndrewFishersb@gmail.com>
2016-10-28 10:17:39 -0700

    display the new question form on the front page

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  newQuestionFormClicked: false,
+  actions: {
+    submitQuestion(){
+      var params = {
+        question: this.get('question')?this.get('question'):"",
+        user: this.get('user')?this.get('user'):"",
+        content: this.get('content')?this.get('content'):"",
+      }
+      this.set('question',"");
+      this.set('user',"");
+      this.set('content',"");
+      this.sendAction("submitQuestion",params);
+    }
+
+
+    hideQuestionForm(){
+      newQuestionFormClicked=false;
+    }
+    showQuestionForm(){
+      newQuestionFormClicked = true;
+    }
+  }
 });
