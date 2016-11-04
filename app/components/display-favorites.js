cf532bbfed5b1cd9e03769dfc7e2c1181aa3629f
Andrew Fisher <AndrewFishersb@gmail.com>
2016-11-04 09:20:53 -0700

    display the favorited questions and display a button for the user to unfavorite the question

--- app/components/display-favorites.js
+++ app/components/display-favorites.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteQuestions: Ember.inject.service(),
+  actions: {
+    removeFromFavorites(question){
+      this.get('favoriteQuestions').remove(question);
+    }
+  }
 });
