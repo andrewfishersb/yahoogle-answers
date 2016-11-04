6fbe588bf3142e2429388fb1954ab56540e5eefd
Andrew Fisher <AndrewFishersb@gmail.com>
2016-11-04 14:53:58 -0700

    create a table that will store questions that people reported as abusive

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   favoriteQuestions: Ember.inject.service(),
+  adminCensor: Ember.inject.service(),
   unFavoriteBox: false,
   actions: {
     addToFavorites(question){
@@ -11,6 +12,10 @@ export default Ember.Component.extend({
     removeFromFavorites(question){
       this.set('unFavoriteBox',false);
       this.get('favoriteQuestions').remove(question);
+    },
+    reportAbuse(entry){
+      alert("An Admin will look into this report momentarily")
+      this.get('adminCensor').add(entry)
     }
   }
 });
---
+++ app/services/admin-censor.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  reportedEntries: [],
+
+  add(entry){
+    this.get('reportedEntries').pushObject(entry);
+  },
+  remove(entry){
+    this.get('reportedEntries').removeObject(entry)
+  }
+});
