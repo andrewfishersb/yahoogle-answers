984d6009f2d861e408af79a23e204cbf9471244e
Andrew Fisher <AndrewFishersb@gmail.com>
2016-11-04 15:37:51 -0700

    Reported answer table displays all items correctly

--- app/components/display-admin.js
+++ app/components/display-admin.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   adminCensor: Ember.inject.service(),
-  adminCensorAnswers: Ember.inject.service()
+  adminCensorAnswers: Ember.inject.service(),
+  actions: {
+    dismissAnswerClaim(answer){
 
+    },
+    dismissQuestionClaim(){
+      
+    }
+  }
 });
