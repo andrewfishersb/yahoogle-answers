0dc8ab6f5e18cc915acd9aa4da35295b4a0358ce
Andrew Fisher <AndrewFishersb@gmail.com>
2016-11-04 09:00:57 -0700

    successfully add a question to a favorites list. Add a route favorite-questions, and a display-favorites component

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteQuestions: Ember.inject.service(),
+  actions: {
+    addToFavorites(question){
+      this.get('favoriteQuestions').add(question);
+    },
+  }
 });
